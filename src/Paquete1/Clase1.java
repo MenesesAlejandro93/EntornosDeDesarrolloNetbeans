81a0f57e64f895c566ca825f9c4d608403944ada
orion <orion@DESKTOP-HUI8TTJ>
2024-01-25 18:16:49 +0100

    commit1

---
+++ src/Paquete1/Clase1.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Paquete1;
+
+/**
+ * algo
+ * @author orion
+ */
+public class Clase1 {
+
+}
